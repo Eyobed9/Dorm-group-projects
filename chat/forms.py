86347d70373e7139cda6604bf381237fe9f5c75a
henok dagne <henokdagne19@gmail.com>
2025-03-19 17:17:52 +0300

    add froms.py for use registration

--- chat/forms.py
+++ chat/forms.py
@@ -22,3 +22,4 @@ class UserRegistrationForm(UserCreationForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords do not match.")
         return password2
+        
